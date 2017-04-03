f837bc56260f73b49529e2a51127ff0fc550596c
ellivr <elliv@gmx.com>
2017-04-03 21:46:30 +0700

    test

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var app = express();
+// Load the http module to create an http server.
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
 
-
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
